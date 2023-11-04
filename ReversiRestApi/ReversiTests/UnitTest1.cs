2408be6bd2ed780c85a4cc02f9a2158559ee6bf1
ThomasF-boop <74776629+ThomasF-boop@users.noreply.github.com>
2023-11-04 16:44:54 +0100

    Level 1

---
+++ ReversiRestApi/ReversiTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ReversiTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
